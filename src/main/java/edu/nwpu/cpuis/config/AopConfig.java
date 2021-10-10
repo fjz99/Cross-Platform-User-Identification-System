cc2816007167b9c4dfedcd89de0e892d455f8806
fjz <1358925318@qq.com>
2021-10-10 19:37:12 +0800

    ok

---
+++ src/main/java/edu/nwpu/cpuis/config/AopConfig.java
@@ -0,0 +1,15 @@
+package edu.nwpu.cpuis.config;
+
+import edu.nwpu.cpuis.aop.ControllerLoggingAspect;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AopConfig {
+    @ConditionalOnProperty(name = "controller-logging", havingValue = "true")
+    @Bean
+    public ControllerLoggingAspect controllerLoggingAspect() {
+        return new ControllerLoggingAspect ();
+    }
+}
