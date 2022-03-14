8afb07c881704782fcecc70558f0e5a8846f9eff
fjz <1358925318@qq.com>
2022-03-14 21:18:02 +0800

    update

--- docker/linux-start.sh
+++ docker/linux-start.sh
@@ -2,12 +2,6 @@
 
 cd ..
 
-#a=$( mvn -q -N -Dexec.executable="echo"  -Dexec.args='${project.version}'  org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
-#b=$( mvn -q -N -Dexec.executable="echo"  -Dexec.args='${project.artifactId}'  org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
-#export JAR_NAME=${b}-${a}
-
-#echo JAR_NAME="$JAR_NAME"
-
 mvn package
 
 docker-compose -f ./docker/docker-compose.yml build
--- docker/linux-stop.sh
+++ docker/linux-stop.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env bash
 
-cd ..
-
-a=$( mvn -q -N -Dexec.executable="echo"  -Dexec.args='${project.version}'  org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
-b=$( mvn -q -N -Dexec.executable="echo"  -Dexec.args='${project.artifactId}'  org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
-export JAR_NAME=${b}-${a}
-
-echo JAR_NAME="$JAR_NAME"
-
-docker-compose -f ./docker/docker-compose.yml stop
+docker-compose -f ./docker-compose.yml down
