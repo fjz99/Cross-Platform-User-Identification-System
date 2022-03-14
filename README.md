030e4e51662c34c0f45755d4368cbde6a8f43bbb
fjz <1358925318@qq.com>
2022-03-14 21:13:02 +0800

    deploy

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ mvn package && java -server -jar target/CPUIS.jar
 Using -Dserver.port property to modify server port(default 8899).
 
 Modify application-data.properties to customize your mongoDB configuration(host or username etc.).
+
+maven 打包默认会加上前端，需要手动排除前端
--- docker/linux-start.sh
+++ docker/linux-start.sh
@@ -2,10 +2,13 @@
 
 cd ..
 
-a=$( mvn -q -N -Dexec.executable="echo"  -Dexec.args='${project.version}'  org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
-b=$( mvn -q -N -Dexec.executable="echo"  -Dexec.args='${project.artifactId}'  org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
-export JAR_NAME=${b}-${a}
+#a=$( mvn -q -N -Dexec.executable="echo"  -Dexec.args='${project.version}'  org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
+#b=$( mvn -q -N -Dexec.executable="echo"  -Dexec.args='${project.artifactId}'  org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
+#export JAR_NAME=${b}-${a}
 
-echo JAR_NAME="$JAR_NAME"
+#echo JAR_NAME="$JAR_NAME"
 
+mvn package
+
+docker-compose -f ./docker/docker-compose.yml build
 docker-compose -f ./docker/docker-compose.yml up -d
